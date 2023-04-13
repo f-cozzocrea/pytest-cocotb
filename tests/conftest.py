19ac286a26a7a31f428d3464bfcab250b82d4a33
Todd Cozzocrea <todd.cozzocrea@biotronik.com>
2023-04-13 13:02:39 -0700

    cli options added in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,64 @@ import os
 from multiprocessing.managers import BaseManager
 from queue import Queue
 
+def pytest_addoption(parser):
+    group = parser.getgroup("pytest-cocotb")
+    group.addoption(
+        "--sim",
+        "--simulator",
+        action="store",
+        dest="sim",
+        default=None,
+        help="sets the default SIM variable used by cocotb. eg. 'icarus', 'ghdl', etc.",
+    )
+    group.addoption(
+        "--top-level",
+        action="store",
+        dest="top-level",
+        default=None,
+        help="sets the top level of HDL that is used",
+    )
+    group.addoption(
+        "--seed",
+        "--random-seed",
+        action="store",
+        dest="seed",
+        default=None,
+        help="set the default seed that's used for test(s)",
+    )
+    group.addoption(
+        "--cocotb-log-level",
+        action="store",
+        dest="cocotb-log-level",
+        default=None,
+        help="sets the log level used by cocotb",
+    )
+    group.addoption(
+        "--gui",
+        action="store",
+        dest="gui",
+        default=None,
+        help="enables the use of a gui mode in simulator that support it",
+    )
+    group.addoption(
+        "--waves",
+        action="store",
+        dest="waves",
+        default=None,
+        help="Set this to 1 to enable wave traces dump for the Aldec Riviera-PRO and Mentor Graphics Questa simulators",
+    )
+    ## TODO: Add additional options. Might be nice to support all environment variables used by cocotb.
+
+
 class QueueManager(BaseManager): pass
+
+@pytest.fixture()
+def sim(config):
+    sim_val = config.getoption("sim")
+    if sim_val is None:
+        sim_val = os.getenv("SIM")
+    os["SIM"] = sim_val
+    return sim_val
 
 @pytest.fixture(scope="session")
 def cocotb_connection(worker_id):
